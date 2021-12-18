d0a218d8f9ff8f9c085806bcccaa3ef9696b4b22
Bertrand <bdelanls@free.fr>
2021-12-18 23:19:59 +0100

    correction images webp

--- src/Service/ImageService.php
+++ src/Service/ImageService.php
@@ -23,20 +23,25 @@ class ImageService
         $imgWide = new ImageManager();
         $imgThumbnail = new ImageManager();
 
+        // le nom de l'image sans l'extension
+        $image = substr($image, 0, strpos($image, '.'));
 
-        $imageOrigin = $this->directoryOrigin . $image;
 
-        $imageWide = $this->directoryWide . $image;
-        $imageThumbnail = $this->directoryThumbnail . $image;
+        $imageOrigin = $this->directoryOrigin . $image . '.jpg';
+
+        $imageWide = $this->directoryWide . $image . '.webp';
+        $imageThumbnail = $this->directoryThumbnail . $image . '.webp';
 
         $imgWide
             ->make($imageOrigin)
             ->fit(800, 480)
+            ->encode('webp', 80)
             ->save($imageWide);
 
         $imgThumbnail
             ->make($imageOrigin)
             ->fit(365, 220)
+            ->encode('webp', 80)
             ->save($imageThumbnail);    
 
     }
