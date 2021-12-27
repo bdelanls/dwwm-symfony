370c27759c1bf557d5567a49b805a5372dc33e85
Bertrand <bdelanls@free.fr>
2021-12-27 10:33:34 +0100

    security

--- assets/script.js
+++ assets/script.js
@@ -30,6 +30,82 @@ window.onresize = () =>{
 };
 
 
+// articles
+if (document.querySelector('section').className === 'article'){
+    const btPrevious = document.querySelector('.previous a');
+    const btNext = document.querySelector('.next a');
+
+    if (btPrevious) {
+        btPrevious.innerHTML = '<svg><title>Article précédent</title><use xlink:href="/img/icons.svg#angle-left"></use></svg>';
+    }
+    if (btNext) {
+        btNext.innerHTML = '<svg><title>Article suivant</title><use xlink:href="/img/icons.svg#angle-right"></use></svg>';
+    }
+}
+
+
+// plus d'articles
+if (document.querySelector('section').className === 'liste-articles'){
+    
+    const btArticles = document.getElementById("load-articles");
+    const list = document.getElementById("list");
+    var page = 1;
+    var totalArticles = list.dataset.totalArticles ;
+    var limit = list.dataset.limit ;
+
+    var nbPages = Math.ceil(totalArticles / limit) ;
+
+
+    function get(url) {
+        return new Promise((resolve, reject) => {
+            const req = new XMLHttpRequest();
+            req.open('GET', url);
+            req.onload = () => req.status === 200 ? resolve(req.response) : reject(Error(req.statusText));
+            req.onerror = (e) => reject(Error(`Network Error: ${e}`));
+            req.send();
+        });
+    }
+
+    btArticles.addEventListener('click', e => {
+
+        page++ ;
+        get("/articles/ajax?page=" + page)
+
+        .then((data) => {
+
+            data = JSON.parse(data);
+
+            for(i = 0; i < data.length; i++) { 
+                
+                let article = data[i];
+
+                list.innerHTML += '<li class="item">'
+                + '<a href="/article/' + article['slug'] + '">'
+                + '<picture>'
+                + '<source srcset="/img/article/thumbnail/'+ article['photoPath'] +'.webp" type="image/webp">'
+                + '<source srcset="/img/article/thumbnail/'+ article['photoPath'] +'.jpg" type="image/jpeg">'
+                + '<img src="/img/article/thumbnail/'+ article['photoPath'] +'.jpg" alt="'+ article['photoTitle'] +'">'
+                + '</picture>'
+                + '<h3>'+ article['title'] +'</h3>'
+                + '</a>'
+                + '<p>'+ article['body']
+                + '</li>';
+        }
+        
+        })
+        .catch((err) => {
+            alert('Une erreur est survenue !');
+        });
+
+        if (page === nbPages ) {
+            btArticles.classList.remove("active");
+        }
+
+    });
+
+}
+
+
 
 
 
